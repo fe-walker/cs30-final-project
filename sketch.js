fd02732181b2e314bbe4b2fecb16b63b5159caad
fe-walker <fewalker.ca@gmail.com>
2025-04-28 14:02:59 -0600

    added lots of text. font, new state variable

--- sketch.js
+++ sketch.js
@@ -5,10 +5,12 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let name;
 let outlaw;
 let backDrop;
 let backDrop3;
 let backDrop2;
+let myFont;
 let xSpot = 50;
 let ySpot;
 let screen = "start";
@@ -18,6 +20,7 @@ function setup() {
 }
 
 function preload(){
+  myFont = loadFont("Ewert-Regular.ttf");
   outlaw = loadImage("sprite_05.png");
   backDrop2 = loadImage("instructions-backdrop.jpg");
   backDrop3 = loadImage("desert.png");
@@ -29,7 +32,6 @@ function preload(){
 
 function draw() {
   // want to rotate through background based on time of day/area?
-
   changeScreenIfNeeded();
   console.log(screen);
 }
@@ -42,6 +44,11 @@ function changeScreenIfNeeded(){
   if (screen === "instructions"){
     // display instructions
     background(backDrop2);
+    displayInstructions();
+  }
+  if (screen === "intro"){
+    background(backDrop2);
+    displayIntro();
   }
   if (screen === "play"){
     // displays game
@@ -49,6 +56,53 @@ function changeScreenIfNeeded(){
     displayEnvironment();
     displayPlayer();
   }
+}
+
+function displayInstructions(){
+  fill("white");
+  textSize(36);
+  textAlign(CENTER);
+  textFont(myFont);
+  text("HOW TO PLAY", width/2, 100);
+
+  textSize(24);
+  text("MOVEMENT/CONTROLS", width/4, 200);
+  text("GOAL", width/4 *3, 200);
+
+  textSize(20);
+  text("Use 'a' & 'd' keys to move forward and backward", width/4, 300);
+  text("Spacebar to jump", width/4, 350);
+  text("Use 'w' & 's' keys to pick up and drop items", width/4, 400);
+  text("There will be frequent pop-ups with text or questions", width/4, 450);
+  text("y = yes n = no, arrow keys will cue text to continue", width/4, 500);
+  text("You are an outlaw in the wildwest", width/4 * 3, 300);
+  text("Your goal is to travel across the desert", width/4 * 3, 350);
+  text("And uncover local mysteries", width/4 * 3, 400);
+  text("Speak to town locals and avoid sheriffs and officals", width/4 * 3, 450);
+  text("Collect bounties and save damsels", width/4 * 3, 500);
+}
+
+function displayIntro(){
+  fill("white");
+  textSize(36);
+  textAlign(CENTER);
+  textFont(myFont);
+  text("WELCOME TO THE WILD WEST", width/2, height/2);
+
+  // move onto each piece of text when forward arrow is pressed (find a way to do this wihtout using clear function)
+  text("PLEASE ENTER YOUR NAME", width/2, height/2);
+  name = prompt("What is your name?");
+  text("You are " + name + " an outlaw notorious for evading the law", width/2, height/2);
+  text("You move around from town to town across the desert, never tied down", width/2, height/2);
+  text("You are a lone wolf and hate having people tag along on your journey", width/2, height/2);
+  text("Many of the folks you've encountered don't have fond memories of you", width/2, height/2);
+  text("Yet you'll always get the job done", width/2, height/2);
+  text("Your main motivation has always been money", width/2, height/2);
+  text("And you've frequently worked as a bounty hunter", width/2, height/2);
+  text("This is a journey of self discovery", width/2, height/2);
+  text("As you discover how truly wonderful the world and humanity can be", width/2, height/2);
+  text("The people you meet can change your cynical attitude, if you allow them", width/2, height/2);
+
 }
 
 function displayEnvironment(){
@@ -86,7 +140,10 @@ function keyPressed(){
     screen = "instructions";
   }
   if (keyCode === 13){
-    screen === "play";
+    screen = "intro";
+  }
+  if (keyCode === 39){
+    // change the text
   }
 }
 
